22fb5a15ab68866efdaba302ab63c0912855bb10
Paulo Guimarães <elmuchacho@outlook.pt>
2024-09-12 15:15:47 +0100

    aula13.py

---
+++ PythonCourse/aula13.py
@@ -0,0 +1,7 @@
+for c in range(1, 6):
+    print(f'{c}- Olá')
+for c in range(6, 0, -1):
+    print(c)
+print('FIM')
+for c in range(0, 7, 2):
+    print(c)
---
+++ PythonCourse/aula13d.py
@@ -0,0 +1,5 @@
+s = 0
+for c in range(0, 5):
+    n = int(input('Digita um valor: '))
+    s += n
+print(f'O somatório de todos os número foi {s}.')
